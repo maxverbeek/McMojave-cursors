84ca3b9ff52c07c117ee5d15c768692e0f57b995
Max Verbeek <m4xv3rb33k@gmail.com>
2024-04-23 22:15:20 +0200

    feat: add flake configuration

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    hyprcursor.url = "github:hyprwm/hyprcursor";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, hyprcursor, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ hyprcursor.outputs.overlays.default ];
+        };
+      in {
+        devShell = pkgs.mkShell {
+          name = "devshell";
+          packages = [ pkgs.hyprcursor ];
+        };
+      });
+}
